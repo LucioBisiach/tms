86e214810457a42a165802b54bc372acb98eaffa
LucioBisiach <l.bisiach@gmail.com>
2021-01-18 15:22:19 +0000

    modificaciones vencimientos

--- models/documents.py
+++ models/documents.py
@@ -10,54 +10,33 @@ from datetime import datetime, date, timedelta
 import logging
 _logger = logging.getLogger(__name__)
 
-class expiryDocumentsTms(models.Model):
-    _name = 'expiry.documents.tms'
-    _description = ''
 
-    name = fields.Char(string="Documento", required=True, copy=False)
-    
-    tipe = fields.Selection([
-        ('company','Compañia'),
-        ('employee','Empleado'),
-        ('fleet','Flota')], string="Tipo", required=True)
-    
-    fleet = fields.Many2one('fleet.vehicle', string="Vehiculo")
-    
-    employee = fields.Many2one('hr.employee', string="Empleado")
-    
-    frecuencia_notifiacion = fields.Selection([
-        ('everyday', 'Todos los días'),
-        ('expiry_date', 'Dia del Vencimiento')], string="Frecuencia Notificación")
-    
-    days_notification = fields.Integer(string="Días de aviso")
-    
-    notificacion_compania = fields.Char(string="Notificar a:")
-    
-    
-    @api.onchange('tipe')
-    def _hide_field(self):
-        if self.tipe == 'company':
-            self.fleet = ''
-            self.employee = ''
-        if self.tipe == 'employee':
-            self.fleet = ''
-        if self.tipe == 'employee':
-            self.employee = ''
-            
-            
-    list_expiry_document = fields.One2many('list.expiry.documents.tms', 'name', string="Vencimiento", ondelete="cascade")
-    
 class ExpiryDocumentAttachment(models.Model):
     _inherit = 'ir.attachment'
 
-    doc_attach_expiry_rel = fields.Many2many('list.expiry.documents.tms', 'expiry_doc_attachment_id', 'attach_id3', 'doc_id',
+    doc_attach_expiry_rel = fields.Many2many('vencimientos.tms', 'vencimientos_doc_attachment_id', 'attach_id3', 'doc_id',
                                     string="Adjunto", invisible=1)
 
-class listExpiryDocumentsTms(models.Model):
-    _name = 'list.expiry.documents.tms'
+
+class TypeExpiryDocumentsTms(models.Model):
+    _name = 'type.vencimientos.tms'
+
+    name = fields.Char(string="Documento")
+
+    tipe = fields.Selection([
+        ('company','Compañia'),
+        ('employee','Empleado'),
+        ('fleet','Flota'),
+        ('otros', 'Otros')], string="Tipo", required=True)
+
+    days_notification = fields.Integer(string="Días de Aviso") 
+
+class ExpiryDocumentsTms(models.Model):
+    _name = 'vencimientos.tms'
     _description = ''
     
-    name = fields.Many2one('expiry.documents.tms', string="Descripción", readonly=True)
+    name = fields.Many2one('type.vencimientos.tms', string="Descripción")
+    
     date_stop = fields.Date(string="Fecha de Vencimiento")
     
     days_remaining = fields.Integer(string="Días Restantes", compute="_get_days_remaining")
@@ -74,19 +53,11 @@ class listExpiryDocumentsTms(models.Model):
     doc_expiry_attachment_id = fields.Many2many('ir.attachment', 'doc_attach_expiry_rel', 'doc_id', 'attach_id3', string="Adjunto",
                                         help='You can attach the copy of your document', copy=False)
     
-    tipe = fields.Char(string="Tipo", invisible=True)
-    document_name = fields.Char(string="Nombre Doc", compute="_get_document_name")
+    tipe = fields.Char(string="Tipo", invisible=True, readonly=True)
     
-    def _get_document_name(self):
-        for rec in self:
-            if rec.name.tipe == 'fleet':
-                rec.document_name = rec.name.fleet.name
-            if rec.name.tipe == 'employee':
-                rec.document_name = rec.name.employee.name
-            if rec.name.tipe == 'company':
-                rec.document_name = 'Company'
-            
+    fleet = fields.Many2one('fleet.vehicle', string="Vehiculo")
     
+    employee = fields.Many2one('hr.employee', string="Empleado")
     
     def _get_status(self):
         for rec in self:
@@ -121,106 +92,106 @@ class listExpiryDocumentsTms(models.Model):
                 rec.days_remaining = 0
                 
                 
-    def notificacion_vencimiento(self):
-        vencimientos_no_archivados = self.search([('archived','!=','True')])
-        now = datetime.now() 
-        date_now = now.date()
-        for i in vencimientos_no_archivados:
+    # def notificacion_vencimiento(self):
+    #     vencimientos_no_archivados = self.search([('archived','!=','True')])
+    #     now = datetime.now() 
+    #     date_now = now.date()
+    #     for i in vencimientos_no_archivados:
             
-            if i.date_stop:
-                # Vencimientos proximo a vencer, con la alerta de todos los dias
-                if i.name.frecuencia_notifiacion == 'everyday' and i.state == 'next' and i.name.tipe == 'employee':
-                    contenido_email = "  Hola  " + i.name.employee.name + " , Tu documento  " + i.name.name + " vence el: " +  str(i.date_stop) + ". Te quedan " + str(abs(i.days_remaining)) + " días para renovarlo. Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s %sproximo a vencer %s') % (i.name.name, i.name.employee.name, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.employee.work_email,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #         if i.date_stop:
+    #             # Vencimientos proximo a vencer, con la alerta de todos los dias
+    #             if i.name.frecuencia_notifiacion == 'everyday' and i.state == 'next' and i.name.tipe == 'employee':
+    #                 contenido_email = "  Hola  " + i.name.employee.name + " , Tu documento  " + i.name.name + " vence el: " +  str(i.date_stop) + ". Te quedan " + str(abs(i.days_remaining)) + " días para renovarlo. Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s %sproximo a vencer %s') % (i.name.name, i.name.employee.name, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.employee.work_email,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'next' and i.name.tipe == 'fleet':
-                    contenido_email = "  Hola, Tu documento  " + i.name.name + " correspondiente al vehículo "+ i.name.fleet.license_plate + " vence el: " +  str(i.date_stop) + ". Te quedan " + str(abs(i.days_remaining)) + " días para renovarlo. Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s %s proximo a vencer %s') % (i.name.name, i.name.fleet.license_plate, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.fleet.email_notificacion,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'next' and i.name.tipe == 'fleet':
+    #                 contenido_email = "  Hola, Tu documento  " + i.name.name + " correspondiente al vehículo "+ i.name.fleet.license_plate + " vence el: " +  str(i.date_stop) + ". Te quedan " + str(abs(i.days_remaining)) + " días para renovarlo. Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s %s proximo a vencer %s') % (i.name.name, i.name.fleet.license_plate, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.fleet.email_notificacion,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'next' and i.name.tipe == 'company':
-                    contenido_email = "  Hola, Tu documento  " + i.name.name + " vence el: " +  str(i.date_stop) + ". Te quedan " + str(abs(i.days_remaining)) + " días para renovarlo. Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s proximo a vencer %s') % (i.name.name, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.notificacion_compania,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'next' and i.name.tipe == 'company':
+    #                 contenido_email = "  Hola, Tu documento  " + i.name.name + " vence el: " +  str(i.date_stop) + ". Te quedan " + str(abs(i.days_remaining)) + " días para renovarlo. Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s proximo a vencer %s') % (i.name.name, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.notificacion_compania,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                # Vencimientos vence hoy, con la alerta de todos los dias
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'today' and i.name.tipe == 'employee':
-                    contenido_email = "  Hola  " + i.name.employee.name + " , Tu documento  " + i.name.name + " vence hoy ! " +  str(i.date_stop) + ". Te quedan " + ". Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s %sproximo a vencer %s') % (i.name.name, i.name.employee.name, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.employee.work_email,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             # Vencimientos vence hoy, con la alerta de todos los dias
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'today' and i.name.tipe == 'employee':
+    #                 contenido_email = "  Hola  " + i.name.employee.name + " , Tu documento  " + i.name.name + " vence hoy ! " +  str(i.date_stop) + ". Te quedan " + ". Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s %sproximo a vencer %s') % (i.name.name, i.name.employee.name, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.employee.work_email,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'today' and i.name.tipe == 'fleet':
-                    contenido_email = "  Hola, Tu documento  " + i.name.name + " correspondiente al vehículo "+ i.name.fleet.license_plate + ". Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s %s vence hoy! %s') % (i.name.name, i.name.fleet.license_plate, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.fleet.email_notificacion,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'today' and i.name.tipe == 'fleet':
+    #                 contenido_email = "  Hola, Tu documento  " + i.name.name + " correspondiente al vehículo "+ i.name.fleet.license_plate + ". Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s %s vence hoy! %s') % (i.name.name, i.name.fleet.license_plate, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.fleet.email_notificacion,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'today' and i.name.tipe == 'company':
-                    contenido_email = "  Hola, Tu documento  " + i.name.name + " vence hoy ! " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s vence hoy! %s') % (i.name.name, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.notificacion_compania,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'today' and i.name.tipe == 'company':
+    #                 contenido_email = "  Hola, Tu documento  " + i.name.name + " vence hoy ! " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s vence hoy! %s') % (i.name.name, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.notificacion_compania,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                # Vencimientos vencido, con la alerta de todos los dias
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'defeated' and i.name.tipe == 'employee':
-                    contenido_email = "  Hola  " + i.name.employee.name + " , Tu documento  " + i.name.name + " se encuentra vencido. Expiró el: " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s - %s Vencido el: %s') % (i.name.name, i.name.employee.name, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.employee.work_email,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             # Vencimientos vencido, con la alerta de todos los dias
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'defeated' and i.name.tipe == 'employee':
+    #                 contenido_email = "  Hola  " + i.name.employee.name + " , Tu documento  " + i.name.name + " se encuentra vencido. Expiró el: " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s - %s Vencido el: %s') % (i.name.name, i.name.employee.name, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.employee.work_email,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'defeated' and i.name.tipe == 'fleet':
-                    contenido_email = "  Hola, Tu documento  " + i.name.name + " correspondiente al vehículo "+ i.name.fleet.license_plate + " vence hoy ! " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s - %s Vencido el: %s') % (i.name.name, i.name.fleet.license_plate, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.fleet.email_notificacion,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'defeated' and i.name.tipe == 'fleet':
+    #                 contenido_email = "  Hola, Tu documento  " + i.name.name + " correspondiente al vehículo "+ i.name.fleet.license_plate + " vence hoy ! " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s - %s Vencido el: %s') % (i.name.name, i.name.fleet.license_plate, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.fleet.email_notificacion,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'defeated' and i.name.tipe == 'company':
-                    contenido_email = "  Hola, Tu documento  " + i.name.name + " venció el:  " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
-                    datos_email = {
-                        'subject': _('Documento-%s Vencido el %s') % (i.name.name, i.date_stop),
-                        'author_id': self.env.company.id,
-                        'body_html': contenido_email,
-                        'email_to': i.name.notificacion_compania,
-                    }
-                    self.env['mail.mail'].create(datos_email).send()
+    #             elif i.name.frecuencia_notifiacion == 'everyday' and i.state == 'defeated' and i.name.tipe == 'company':
+    #                 contenido_email = "  Hola, Tu documento  " + i.name.name + " venció el:  " +  str(i.date_stop) + ". Porfavor actualizalo lo antes posible."
+    #                 datos_email = {
+    #                     'subject': _('Documento-%s Vencido el %s') % (i.name.name, i.date_stop),
+    #                     'author_id': self.env.company.id,
+    #                     'body_html': contenido_email,
+    #                     'email_to': i.name.notificacion_compania,
+    #                 }
+    #                 self.env['mail.mail'].create(datos_email).send()
                     
-                # Vencimiento hoy con alerta de hoy
+    #             # Vencimiento hoy con alerta de hoy
 
-                # elif date_now == i.date_stop and i.name.frecuencia_notifiacion == 'expiry_date':
+    #             # elif date_now == i.date_stop and i.name.frecuencia_notifiacion == 'expiry_date':
